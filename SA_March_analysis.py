8ffc160c58680afaf7319bb6ff2238a505470406
mckabuza <kablokes@gmail.com>
2020-04-16 02:06:44 +0200

    Third commit

--- SA_March_analysis.py
+++ SA_March_analysis.py
@@ -34,6 +34,7 @@ all_colors = list(plt.cm.colors.cnames.keys())
 random.seed(100)
 c = random.choices(all_colors, k=len(dates))
 
+fig = plt.figure(1)
 plt.title('South Africa COVID-19 March Infections by day in March 2020')
 plt.plot(dates, daily_infections,'r-o',color='dodgerblue') #line plot
 plt.bar(dates, daily_infections,color=c)
@@ -44,56 +45,51 @@ plt.yticks(range(0,1500, 100))
 plt.ylabel('Number of positive COVID-19 cases')
 #plt.ylim(0,1200)
 plt.grid(axis='y')
-#plt.savefig("Infections.jpg")
-plt.savefig('daily.jpg')
+#plt.savefig('daily_infections.jpg')
 
 weekly = [1,37,202,1113]
 weeks = ['Week 1','Week 2','Week 3','Week 4']
-
-plt.title('Number of confirmed positive cases\nfor each week in March as at March 30 Noon')
+fig = plt.figure(2)
+plt.title('Number of confirmed positive cases for each week in March')
 plt.bar(weeks,weekly,width=0.45,color='teal')
 plt.ylabel('Number of positive COVID-19 cases')
 for i,weekly in enumerate(weekly):
     plt.text(i, weekly+10, weekly, horizontalalignment='center')
-plt.savefig('weekly_stats.jpg')
+#plt.savefig('weekly_stats.jpg')
 '''
 #PROVINCIAL ANALYSIS
 '''
-province = ['Free\nState','Eastern\nCape','Northern\nCape','Western\nCape','Gauteng',
-            'Kwa-Zulu\nNatal','North\nWest','Limpopo','Mpuma-\nlanga', 'Province\nUnknown']
-
-province_infections = [74, 12, 6, 325, 633, 179, 8, 14, 12, 90]
+province = ['Free\nState','Mpuma-\nlanga','Western\nCape','Eastern\nCape','Kwa-Zulu\nNatal','North\nWest','Gauteng','Northern\nCape','Province\nUnknown','Limpopo']
+province_infections = [74,12,325,12,179,8,633,6,90,14]
 
 '''
 provincial = df({'Province':['Free State','Eastern Cape','Northern Cape','Western Cape','Gauteng',
             'Kwa-Zulu Natal','North West','Limpopo','Mpumalanga', 'Province Unknown'],
             'Positive Cases':[74, 12, 6, 325, 633, 179, 8, 14, 12, 90]})
 '''
+fig = plt.figure(3)
 plt.title('Provincial breakdown of COVID-19 March 2020 South Africa')
-plt.bar(province, province_infections, color = 'steelblue')
+plt.bar(province, province_infections, color = c)
 plt.ylabel('No. of positive cases')
 for j,province_infections in enumerate(province_infections):
     plt.text(j,province_infections+2, province_infections,horizontalalignment='center')
 plt.xticks(rotation=55)
-plt.savefig('provincial_plot.jpg')
+#plt.savefig('provincial_plot.jpg')
 
-prov = ['Free\nState','Mpumalanga','Western\nCape','Eastern\nCape','Kwa-Zulu\nNatal','North\nWest','Gauteng',
-        'Northern\nCape','Province\nUnknown','Limpopo']
-prov_inf = [province_infections[0],province_infections[8], province_infections[3],
-            province_infections[1], province_infections[5], province_infections[6], province_infections[4],
-            province_infections[2], province_infections[9], province_infections[7]]
-
-
-plt.pie(prov_inf, labels=prov, autopct='%1.2f%%')
-plt.savefig('provincial_pie.jpg')
+#plt.pie(province_infections, labels=province, autopct='%1.2f%%')
+#plt.savefig('provincial_plot_pie.jpg')
 
 
 data1 = ['Total\ncases','Recoveries','Patients\nin ICU', 'Patients on\nventilators','Deaths']
 dat1=[1353,31,4,3,5]
-
+fig = plt.figure(5)
 plt.title('Key Statistics (March 30)')
 plt.barh(data1, dat1,color='teal')
 for k,dat1 in enumerate(dat1):
     plt.text(dat1+2,k,dat1,verticalalignment='center')
 
-plt.savefig('key_stats.jpg')
+#plt.savefig('key_stats.jpg')
+    
+for i in range(4):
+    plt.figure(i+1)
+plt.show()
